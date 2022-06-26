ae2dcd0683f5012e8b2861307ae212a7788c00e6
adrian-iglesia-peya <adrian.iglesia@pedidosya.com>
2022-06-26 17:05:21 -0300

    added base ui (list and button)

--- app/src/main/java/com/eidorian/weedtacora/presentation/MainActivity.kt
+++ app/src/main/java/com/eidorian/weedtacora/presentation/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.eidorian.weedtacora.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
-import com.eidorian.weedtacora.data.entities.Growth
-import com.eidorian.weedtacora.data.repository.GrowthRepository
-import com.eidorian.weedtacora.data.repository.GrowthRepositoryImpl
+import androidx.compose.ui.unit.dp
 import com.eidorian.weedtacora.ui.theme.WeedtacoraTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -20,14 +22,12 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @Inject
-    lateinit var repo: GrowthRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WeedtacoraTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MyHomeScreen()
                 }
             }
         }
@@ -35,14 +35,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyHomeScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            GrowthsList()
+        }
+        Row {
+            Footer()
+        }
+    }
+
+}
+
+@Composable
+fun GrowthsList() {
+    LazyColumn{
+        items(15) { i ->
+            Text(
+                "Row $i",
+                Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun Footer() {
+    Button(
+        modifier = Modifier.fillMaxWidth().height(64.dp),
+        shape = RectangleShape,
+        onClick = {},
+    ) {
+        Text(text = "Agregar cultivo")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     WeedtacoraTheme {
-        Greeting("Android")
+        MyHomeScreen()
     }
 }
