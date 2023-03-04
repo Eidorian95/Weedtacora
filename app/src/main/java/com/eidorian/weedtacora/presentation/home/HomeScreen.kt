176895fb88c82719b0c75c333c2eaee76f9dda15
adrian-iglesia-peya <adrian.iglesia@pedidosya.com>
2023-03-04 16:00:58 -0300

    Removed hardcoded growth in repo

--- app/src/main/java/com/eidorian/weedtacora/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/eidorian/weedtacora/presentation/home/HomeScreen.kt
@@ -1,7 +1,9 @@
 package com.eidorian.weedtacora.presentation.home
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -32,6 +34,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
@@ -118,9 +121,9 @@ fun HomeScreenContent(growthList: List<GrowthUiModel>, navController: NavControl
             items(
                 items = growthList,
                 key = { item: GrowthUiModel -> item.id }
-            ) { growthId ->
-                GrowthCard(growth = growthId) {
-                    navController.navigate(Screen.GrowthDetailsScreen.route.plus("/$growthId"))
+            ) { growth ->
+                GrowthCard(growth = growth) {
+                    navController.navigate(Screen.GrowthDetailsScreen.route.plus("/${growth.id}"))
                 }
             }
         }
@@ -128,18 +131,23 @@ fun HomeScreenContent(growthList: List<GrowthUiModel>, navController: NavControl
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun GrowthCard(growth: GrowthUiModel, onClick: () -> Unit) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onClick() }
+            .combinedClickable(
+                onClick = onClick,
+                onLongClick = {}
+            )
             .padding(vertical = 8.dp),
     ) {
         Column(modifier = Modifier.padding(8.dp)) {
             Text(
                 text = growth.name.uppercase(),
                 fontWeight = FontWeight.Bold,
+                color = Color.Black,
                 fontSize = 18.sp,
                 letterSpacing = 1.sp,
                 modifier = Modifier
